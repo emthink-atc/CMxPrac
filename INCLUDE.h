8b6f478091b00cd8e078b4676d35a3d1eeb8d859
sj2456 <john.yoon@emthink.com>
2016-12-13 15:10:42 +0900

    Practical 4

--- INCLUDE.h
+++ INCLUDE.h
@@ -2,19 +2,23 @@
  *      www.emthink.com
  *      (C)2016 Cortex-Mx Practical Course
  *
- * FILE: INCLUDE.h for 3-Blinky
+ * FILE: INCLUDE.h for 4-GLCD
  *----------------------------------------------------------------------------*/
 
 #ifndef __INCLUDE_H
 #define __INCLUDE_H
 
-
 #include "stm32f4xx_hal.h"                  /* STM32F4xx Definitions          */
-#include "Board_LED.h"
 
 #ifdef __MASTER_FILE
 extern void SystemClock_Config(void);
 #endif /* __MASTER_FILE */
 
+#include "Board_LED.h"
+#include "Board_GLCD.h"
+#include "GLCD_Config.h"
+
+/* Font */
+extern GLCD_FONT     GLCD_Font_16x24;
 #endif /* __INCLUDE_H */
 
