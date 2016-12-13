41aced44467456447226eb6cfc4c04e36ca16a4d
sj2456 <john.yoon@emthink.com>
2016-12-13 15:01:59 +0900

    Practical 2

--- Practical.c
+++ Practical.c
@@ -2,19 +2,35 @@
  *      www.emthink.com
  *      (C)2016 Cortex-Mx Practical Course
  *
- * FILE: Practical.c for 1-main
+ * FILE: Practical.c for 2-SysTick
  *----------------------------------------------------------------------------*/
 
 #define __MASTER_FILE
 #include "INCLUDE.h"
 
-
 /*----------------------------------------------------------------------------*
  *      Main function
  *----------------------------------------------------------------------------*/
+
 int main (void) {
   SystemClock_Config();
+  SysTick_Config(SystemCoreClock/100);      /* Generate interrupt each 10 ms  */
 
   while(1);
 }
 
+
+/*----------------------------------------------------------------------------*
+ *      SysTick IRQ Handler
+ *----------------------------------------------------------------------------*/
+
+void SysTick_Handler (void) {
+  static uint32_t ticks;
+  
+  switch (ticks++) {
+    default:
+      if (ticks > 10) {
+        ticks = 0;
+      }
+  }
+}
