af1e6207e91979e0b00ac0ccd9c9bf1e1bc883e9
sj2456 <john.yoon@emthink.com>
2016-12-13 15:05:45 +0900

    Practical 3

--- INCLUDE.h
+++ INCLUDE.h
@@ -2,16 +2,15 @@
  *      www.emthink.com
  *      (C)2016 Cortex-Mx Practical Course
  *
- * FILE: INCLUDE.h for 1-main & 2-SysTick
+ * FILE: INCLUDE.h for 3-Blinky
  *----------------------------------------------------------------------------*/
-
 
 #ifndef __INCLUDE_H
 #define __INCLUDE_H
 
 
 #include "stm32f4xx_hal.h"                  /* STM32F4xx Definitions          */
-
+#include "Board_LED.h"
 
 #ifdef __MASTER_FILE
 extern void SystemClock_Config(void);
--- Practical.c
+++ Practical.c
@@ -2,21 +2,49 @@
  *      www.emthink.com
  *      (C)2016 Cortex-Mx Practical Course
  *
- * FILE: Practical.c for 2-SysTick
+ * FILE: Practical.c for 3-Blinky
  *----------------------------------------------------------------------------*/
+
 
 #define __MASTER_FILE
 #include "INCLUDE.h"
 
+
+uint32_t LEDOn, LEDOff;
+
 /*----------------------------------------------------------------------------*
  *      Main function
  *----------------------------------------------------------------------------*/
-
 int main (void) {
+
+
+  int32_t max_num = LED_GetCount() - 1;
+  int32_t num = 0;
+  int32_t dir = 1;
+
   SystemClock_Config();
   SysTick_Config(SystemCoreClock/100);      /* Generate interrupt each 10 ms  */
+  LED_Initialize();                         /* LED Initialization             */
 
-  while(1);
+  while (1) {
+    if (LEDOn) {
+      LEDOn = 0;
+      LED_On (num);                         /* Turn specified LED on          */
+    }
+
+    if (LEDOff) {
+      LEDOff = 0;
+      LED_Off (num);                        /* Turn specified LED off         */
+      num += dir;                           /* Change LED number              */
+
+      if (dir == 1 && num == max_num) {
+        dir = -1;                           /* Change direction to down       */
+      }
+      else if (num == 0) {
+        dir =  1;                           /* Change direction to up         */
+      }
+    }
+  }
 }
 
 
@@ -26,11 +54,16 @@ int main (void) {
 
 void SysTick_Handler (void) {
   static uint32_t ticks;
-  
+
   switch (ticks++) {
+    case  0: LEDOn  = 1; break;
+    case  5: LEDOff = 1; break;
+    case  9: ticks  = 0; break;
+
     default:
       if (ticks > 10) {
         ticks = 0;
       }
   }
 }
+
