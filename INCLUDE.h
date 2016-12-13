11e48fdb1da4ea90d3482af8a7089d91e0e47ef2
sj2456 <john.yoon@emthink.com>
2016-12-13 14:40:27 +0900

    Practical 1

---
+++ INCLUDE.h
@@ -0,0 +1,21 @@
+/*----------------------------------------------------------------------------*
+ *      www.emthink.com
+ *      (C)2016 Cortex-Mx Practical Course
+ *
+ * FILE: INCLUDE.h for 1-main & 2-SysTick
+ *----------------------------------------------------------------------------*/
+
+
+#ifndef __INCLUDE_H
+#define __INCLUDE_H
+
+
+#include "stm32f4xx_hal.h"                  /* STM32F4xx Definitions          */
+
+
+#ifdef __MASTER_FILE
+extern void SystemClock_Config(void);
+#endif /* __MASTER_FILE */
+
+#endif /* __INCLUDE_H */
+
